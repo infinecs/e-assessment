b145fa2418c40351d25c15dcfb62149c01cdfb0b
Vibhushan Muthuramu <vibhushan.muthuramu@infinecs.com>
2026-06-22 16:13:17 +0800

    Make company logo clickable - redirects to dashboard

--- resources/views/inc/headerMain.blade.php
+++ resources/views/inc/headerMain.blade.php
@@ -5,7 +5,7 @@
         <!-- Left: Logo & Menu Toggle -->
         <div class="flex items-center topbar-brand">
             <div class="hidden lg:flex navbar-brand items-center justify-between shrink px-6 h-[70px] ltr:border-r rtl:border-l bg-[#fbfaff] border-gray-50 dark:border-zinc-700 dark:bg-zinc-800 shadow-none">
-                <a href="#" class="flex items-center text-3xl font-black tracking-tight flex-shrink-0 dark:text-white leading-[69px] px-2 py-1 rounded">
+                <a href="{{ route('assessment.index') }}" class="flex items-center text-3xl font-black tracking-tight flex-shrink-0 dark:text-white leading-[69px] px-2 py-1 rounded hover:opacity-80 transition-opacity" title="Go to Dashboard">
                     <img src="{{ asset('images/Infinecs-with-slogan.png') }}" alt="Infinecs with Slogan" class="h-9 inline">
                 </a>
             </div>
